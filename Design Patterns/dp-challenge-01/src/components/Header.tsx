278aeae32e53e8e4317275f616796b7f02e71a26
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-30 23:26:43 +0530

    feat: Implement Header component with navigation links

--- Design Patterns/dp-challenge-01/src/components/Header.tsx
+++ Design Patterns/dp-challenge-01/src/components/Header.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-green-800 px-4 py-2 flex justify-between items-center '>
+      <Link to='/'>My Dashboard</Link>
+
+      <div className='flex gap-2'>
+        <Link to='/profile'>Profile</Link>
+        <Link to="/logout">Logout</Link>
+      </div>
+    </header>
   )
 }
 
