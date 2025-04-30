53a81e142124b702f77087bdd1e68b01a25caebc
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-30 23:17:57 +0530

    added header and Footer to the Layout

---
+++ Design Patterns/dp-challenge-01/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex '>
+      <h1 className='text-3xl'>Design Pattern Challenge 01</h1>
+    </div>
+  )
+}
+
+export default App
--- Design Patterns/dp-demos/src/components/Right.tsx
+++ Design Patterns/dp-demos/src/components/Right.tsx
@@ -1,9 +1,9 @@
 
 const Right = () => {
   return (
-    <div className='bg-red-400 '>
-      <h1 className="text-3xl text-white text-center p-4">Something coool</h1>
-
+    <div className='bg-red-400 p-2'>
+      <h1 className="text-3xl text-white text-center ">Something coool</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, minima illum? Veritatis ratione consequuntur fugiat saepe laborum architecto iste error incidunt quaerat necessitatibus quae quibusdam, magnam dolore quam corporis modi itaque tenetur sit. Consequuntur!</p>
     </div>
   )
 }
